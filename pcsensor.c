3232b852d37bfa07009dd7d386c5acd2dce05d26
Petri Launimaa <launimaa.petri@gmail.com>
2020-07-06 23:30:25 +0300

    fixed

--- pcsensor.c
+++ pcsensor.c
@@ -435,15 +435,15 @@ int main(int argc, char **argv) {
     do {
         for (i = 0; i < numdev; i++) {
             // get localtime
-/*             t = time(NULL);
+            t = time(NULL);
             local = localtime(&t);
-            sprintf(strdate, "%04d-%02d-%02dT%02d:%02d:%02d",
-                    local->tm_year +1900,
-                    local->tm_mon + 1,
-                    local->tm_mday,
-                    local->tm_hour,
-                    local->tm_min,
-                    local->tm_sec); */
+            // sprintf(strdate, "%04d-%02d-%02dT%02d:%02d:%02d",
+            //         local->tm_year +1900,
+            //         local->tm_mon + 1,
+            //         local->tm_mday,
+            //         local->tm_hour,
+            //         local->tm_min,
+            //         local->tm_sec);
 
             // get temperature
             control_transfer(devices[i].handle, uTemperature);
